e80cc92d6fdd01582aa013044b95b00728acae31
Edwin Galeano <ed.galeano1@gmail.com>
2023-02-01 23:05:02 -0500

    Complete Foundamentals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './product.model';
 
 @Component({
   selector: 'app-root',
@@ -6,13 +7,91 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  widthImg = 10;
   name = 'Nicolas';
   age = 18;
   img = 'https://www.w3schools.com/howto/img_avatar.png';
   btnDisabled = true;
+  register = {
+    name: '',
+    email: '',
+    password: '',
+  };
   person = {
     name: 'Nicolas',
     age: 18,
     avatar: 'https://www.w3schools.com/howto/img_avatar.png'
+  };
+  names: string[] = ['Nicolas', 'Julian', 'Santiago'];
+  newName = '';
+  title: any;
+  box = {
+    width: 100,
+    height: 100,
+    background: 'red'
+  };
+
+  toggleButton() {
+    this.btnDisabled = !this.btnDisabled;
+  }
+
+  products: Product[] = [
+    {
+      name: 'EL mejor juguete',
+      price: 565,
+      image: './assets/images/toy.jpg',
+      category: 'all',
+    },
+    {
+      name: 'Bicicleta casi nueva',
+      price: 356,
+      image: './assets/images/bike.jpg'
+    },
+    {
+      name: 'Colleción de albumnes',
+      price: 34,
+      image: './assets/images/album.jpg'
+    },
+    {
+      name: 'Mis libros',
+      price: 23,
+      image: './assets/images/books.jpg'
+    },
+    {
+      name: 'Casa para perro',
+      price: 34,
+      image: './assets/images/house.jpg'
+    },
+    {
+      name: 'Gafas',
+      price: 3434,
+      image: './assets/images/glasses.jpg'
+    }
+  ]
+
+  addName() {
+    this.names.push(this.newName);
+    this.newName = '';
+  }
+  changeName(event: Event) {
+    const element = event.target as HTMLInputElement;
+    this.person.name = element.value;
+  }
+
+  removeName(i: number) {
+    this.names.splice(i, 1);
+  }
+
+  onScroll(event: Event) {
+    const element = event.target as HTMLElement;
+    console.log(element.scrollTop);
+  }
+
+  increaseAge() {
+    this.person.age++;
+  }
+
+  onRegister() {
+    console.log(this.register);
   }
 }
